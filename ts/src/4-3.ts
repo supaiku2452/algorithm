b05a69435232023afeb7915d83bec0e32010e1dc
supaiku2452 <supaiku2452@gmail.com>
2021-05-05 22:13:04 +0900

    add 4-3.

---
+++ ts/src/4-3.ts
@@ -0,0 +1,21 @@
+// フィボナッチ数列の計算
+
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const fibo = (n: number): number => {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  return fibo(n - 2) + fibo(n - 1);
+};
+
+rl.question("input two number:", (answer) => {
+  const result = fibo(Number(answer));
+  console.log({ result });
+  rl.close();
+});
