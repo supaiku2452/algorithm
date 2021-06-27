afb9f9a403b42cfb9ff2de4278f08f06ec0907d1
supaiku2452 <supaiku2452@gmail.com>
2021-06-27 23:03:44 +0900

    refactor.

--- ts/src/sort/insertion.ts
+++ ts/src/sort/insertion.ts
@@ -1,18 +1,23 @@
 // 挿入ソート(昇順)
 
-let ary = [10, 5, 3, 4, 16, 12];
+let array = [10, 5, 3, 4, 16, 12];
 
-for (let i = 1; i < ary.length; i++) {
-  let value = ary[i];
+for (let i = 1; i < array.length; i++) {
+  const v = array[i]; // 今の値を待避(比較対象)
 
-  let j = i;
+  let j = i; //iいかがソート対象
   for (; j > 0; j--) {
-    if (value < ary[j - 1]) {
-      ary[j] = ary[j - 1];
+    //比較対象が一つ前の要素より小さい場合はjにj-1の値を設定
+    if (v < array[j - 1]) {
+      // 降順にしたい場合はここを反転
+      array[j] = array[j - 1];
     } else {
+      // ifを満たさない場合はそれ以降ソート不要(ソート済みなので)
       break;
     }
   }
-  ary[j] = value;
+  // 最後に一番小さいところ(jの最終結果)に比較対象を入れる
+  array[j] = v;
 }
-console.log(ary.toString());
+
+console.log(array);
